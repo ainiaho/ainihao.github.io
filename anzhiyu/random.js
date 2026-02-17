06a0406db4c31a242cd95bb6fa5afa467a3d65a7
ainihao <a2364324806@gmail.com>
2026-02-17 09:26:48 +0000

    Site updated: 2026-02-17 09:26:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["1970/01/01/hello-world/","2026/02/17/linux/","2026/02/17/My-New-Post/"];function toRandomPost(){
+var posts=["2026/02/17/My-New-Post/","2026/02/17/linux/","1970/01/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
